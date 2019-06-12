951eeef0c0c6bd3767bf55b4826ba20e2ae9f913
Crazy Urus <crazyurus@vip.qq.com>
2019-06-12 23:29:51 +0800

    feat: 校歌模块

--- src/components/Block/index.jsx
+++ src/components/Block/index.jsx
@@ -4,7 +4,10 @@ import styles from './index.less';
 export default memo(props => {
   return (
     <div className={styles.block}>
-      <div className={styles.title}>{props.title}</div>
+      <div className={styles.title}>
+        <span>{props.title}</span>
+        {props.extra}
+      </div>
       <div className={styles.content}>{props.children}</div>
     </div>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -17,23 +17,30 @@ export default () => (
         <ListItem
           title="图书馆"
           icon="book"
+          url="https://mp.weixin.qq.com/s/DNagKucWjdh7guYfNZdqxg"
         />
         <ListItem
           title="食堂"
           icon="tea"
+          url="https://mp.weixin.qq.com/s?__biz=MjM5MjEwNDIwMA==&mid=502148076&idx=1&sn=c0bbbf8e0af8efb40b3b3fa33cedbabb&chksm=3eb1e18b09c6689dfb87be6de903010170d7126fd499aaf8554377ea1596d6d12df4d3a3d04d#rd"
         />
         <ListItem
           title="校车"
           icon="hospital"
+          url="https://mp.weixin.qq.com/s/8LjI4dJVtnxzdSLvitvgjg"
         />
         <ListItem
           title="快递/超市/卡务中心/医院"
           icon="card"
+          url="https://mp.weixin.qq.com/s?__biz=MjM5MjEwNDIwMA==&mid=502148076&idx=1&sn=c0bbbf8e0af8efb40b3b3fa33cedbabb&chksm=3eb1e18b09c6689dfb87be6de903010170d7126fd499aaf8554377ea1596d6d12df4d3a3d04d#rd"
         />
       </List>
     </Block>
 
-    <Block title="校歌">
+    <Block
+      title="校歌"
+      extra={<a href="//m.music.163.com/m/applink/?scheme=orpheus%3A%2F%2Fsong%2F449577766%3Fthirdfrom%3Diwut">打开网易云音乐</a>}
+    >
       <iframe
         className={styles.iframe}
         frameBorder="no"
