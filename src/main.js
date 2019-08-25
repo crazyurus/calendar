ac69afc6b83b68c96b77292f726f15aa03f51b4c
廖星 <liaoxing.lx@bytedance.com>
2019-08-25 17:09:50 +0800

    移除OfflinePluginRuntime

--- src/main.js
+++ src/main.js
@@ -4,16 +4,12 @@ import ReactDOM from 'react-dom';
 import Framework7 from 'framework7/framework7.esm.bundle.js';
 import Framework7React from 'framework7-react';
 
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
-
 import 'framework7/css/framework7.bundle.css';
 import './css/global.less';
 
 import App from './components/App';
 
 Framework7.use(Framework7React);
-
-if (process.env.NODE_ENV === 'production') OfflinePluginRuntime.install();
 
 ReactDOM.render(
   React.createElement(App),
